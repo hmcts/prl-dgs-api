12a1ffca0b71928a4398c16363e1c5831ad53d43
Aloknath Datta <alokdatta.uit@gmail.com>
2022-03-18 00:55:48 +0000

    Code smell fix

--- src/main/java/uk/gov/hmcts/reform/prl/documentgenerator/mapper/TemplateDataMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/documentgenerator/mapper/TemplateDataMapper.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.documentgenerator.mapper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.prl.documentgenerator.config.DocmosisBasePdfConfig;
