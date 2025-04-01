8c7fff5e49cf6dba9abc0e14aff1cac23fdc0ca4
Michael1142 <michael.johnson9@justice.gov.uk>
2025-04-01 09:51:01 +0100

    Fix sonar issues, improve logging and fix typo

--- src/main/java/uk/gov/hmcts/reform/prl/documentgenerator/controller/DocumentGeneratorController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/documentgenerator/controller/DocumentGeneratorController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,64 +30,61 @@ public class DocumentGeneratorController {
     private final DocumentManagementService documentManagementService;
 
     @Operation(description = "Generate PDF document based on the supplied template name and placeholder "
-            + "texts and saves it in the evidence management.", tags = {"Document Generation"})
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "PDF was generated successfully and stored in the "
-                    + "evidence management. Returns the url to the stored document.", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "400", description = "Returned when input parameters are invalid "
-                    + "or template not found", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "503", description = "Returned when the PDF Service or Evidence "
-                    +  "Management Client Api cannot be reached", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "500", description = "Returned when there is an unknown server error",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
-        })
+        + "texts and saves it in the evidence management.", tags = {"Document Generation"})
+    @ApiResponse(responseCode = "200", description = "PDF was generated successfully and stored in the "
+        + "evidence management. Returns the URL to the stored document.",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "400", description = "Returned when input parameters are invalid "
+        + "or template not found",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "503", description = "Returned when the PDF Service or Evidence "
+        + "Management Client API cannot be reached",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "500", description = "Returned when there is an unknown server error",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/generatePDF")
     public GeneratedDocumentInfo generateAndUploadPdf(
         @RequestHeader(value = "Authorization", required = false)
-            String authorizationToken,
+        String authorizationToken,
         @Parameter(name = "GenerateDocumentRequest",description = "JSON object containing "
-                + "the templateName and the placeholder text map", required = true)
+            + "the templateName and the placeholder text map", required = true)
         @RequestBody
         @Valid
-            GenerateDocumentRequest templateData) {
+        GenerateDocumentRequest templateData) {
         //This service is internal to PRL system. No need to do service authentication here
-        log.info("Document generation requested with templateName [{}], placeholders map of size[{}]",
-                templateData.getTemplate(), templateData.getValues().size());
-        return documentManagementService.generateAndStoreDocument(templateData.getTemplate(), templateData.getValues(),
-            authorizationToken);
+
+        log.info("Generating PDF document with templateName [{}] and [{}] placeholders",
+                 templateData.getTemplate(), templateData.getValues().size());
+        return documentManagementService.generateAndStoreDocument(
+            templateData.getTemplate(), templateData.getValues(), authorizationToken
+        );
     }
 
     @Operation(description = "Generate draft PDF document based on the supplied template name and placeholder texts "
-            + "and saves it in the evidence management.", tags = {"Document Generation"})
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "PDF was generated successfully and stored in the "
-                    + "evidence management. Returns the url to the stored document.",content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "400", description = "Returned when input parameters are invalid or "
-                    + "template not found", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "503", description = "Returned when the PDF Service or Evidence Management "
-                    + "Client Api cannot be reached", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "500", description = "Returned when there is an unknown server error",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
-        })
+        + "and saves it in the evidence management.", tags = {"Document Generation"})
+    @ApiResponse(responseCode = "200", description = "PDF was generated successfully and stored in the "
+        + "evidence management. Returns the URL to the stored document.",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "400", description = "Returned when input parameters are invalid or "
+        + "template not found",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "503", description = "Returned when the PDF Service or Evidence Management "
+        + "Client API cannot be reached",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "500", description = "Returned when there is an unknown server error",
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/generateDraftPDF")
     public GeneratedDocumentInfo generateAndUploadDraftPdf(
         @RequestHeader(value = "Authorization", required = false)
-            String authorizationToken,
+        String authorizationToken,
         @Parameter(name = "GenerateDocumentRequest",description = "JSON object containing the "
-                + "templateName and the placeholder text map", required = true)
+            + "templateName and the placeholder text map", required = true)
         @RequestBody
         @Valid
-            GenerateDocumentRequest templateData) {
+        GenerateDocumentRequest templateData) {
         //This service is internal to Divorce system. No need to do service authentication here
-        log.info("Document generation requested with templateName [{}], placeholders map of size[{}]",
-                 templateData.getTemplate(), templateData.getValues().size()
-        );
+        log.info("Generating draft PDF with templateName [{}] and [{}] placeholders",
+                 templateData.getTemplate(), templateData.getValues().size());
         return documentManagementService.generateAndStoreDraftDocument(templateData.getTemplate(),
                                                                        templateData.getValues(), authorizationToken
         );
@@ -106,8 +102,7 @@ public class DocumentGeneratorController {
         GenerateDocumentRequest templateData) {
         //This service is internal to Divorce system. No need to do service authentication here
         log.info("convertDocumentToPdf is getting called");
-        return documentManagementService.converToPdf(
-            templateData.getValues(), authorizationToken, fileName);
+        return documentManagementService.convertToPdf(templateData.getValues(), authorizationToken, fileName);
     }
 
 }
